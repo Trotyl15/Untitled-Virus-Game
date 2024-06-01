f6dab06a16d259cd68e349ddf4fc27b10d600812
Michael Wu <wurunzhi@hotmail.com>
2024-06-01 11:38:07 -0400

    mock tree

--- Scenes/night_and_day.gd
+++ Scenes/night_and_day.gd
@@ -1,9 +1,29 @@
 extends Node2D
 
+const NIGHT_POS = Vector2(576, 324)
+const DAY_POS = Vector2(576, 1200)
 
+func _ready():
+	get_node("Camera2D").position = NIGHT_POS
 
 
 func _on_confirm_pressed():
 	# check if is the correct amount of skill
-	# add skill to virus.gd
-	get_tree().change_scene_to_file("res://Scenes/day.tscn")
+	# add skill to virus.gd (rebuild all skills)
+	get_node("Camera2D").position = DAY_POS
+
+
+func night_phase_prep():
+	if Grid.day == 1:
+		pass
+	if Grid.day == 2:
+		pass
+	else:
+		pass
+
+
+
+
+# func day_end()
+# increase day counter
+# move camera
--- SkillTree/skill_node.gd
+++ SkillTree/skill_node.gd
@@ -9,4 +9,16 @@ func _process(_delta):
 
 
 func box_state():
-	return box.button_pressed
+	if is_instance_valid(box):
+		return box.button_pressed
+	else:
+		return true
+
+
+func lock_in():
+	get_node("ColorRect").queue_free()
+	box.queue_free()
+
+
+func _on_check_box_toggled(toggled_on):
+	get_node("ColorRect").visible = not get_node("ColorRect").visible
